dd888af7f834333e1199705cf27db92ea637b586
Felipe Sousa Chagas <felypegt8@gmail.com>
2024-12-12 10:43:56 -0300

    adicionar fontes a pasta fontes em estilos globais

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+import EstilosGlobais from "./components/EstilosGlobais"
+import Cabecalho from "./components/Cabecalho"
+import SideBar from "./components/SideBar"
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+function App() {
+
+  return (
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+      <SideBar />
+
+    </FundoGradiente>
+  )
+}
+
+export default App
